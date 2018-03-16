a6287a3589c464470b06b102676900208fcf70f2
zzzluyan <zzzluyan@qq.com>
2018-03-16 16:19:00 +0800

    note template test

---
+++ src/components/Note.vue
@@ -0,0 +1,37 @@
+<template>
+	<div id="note">
+		<p id="note-title">{{ NoteTitle }}</p>
+		<p id="note-date">{{ NoteDate }}</p>
+		<button id="delete-note" v-on:click="deleteNote">delete</button>
+		<button id="edit-note" v-on:click="editNote">edit</button>
+		<!-- <div id="note-content">{{ NoteContent }}</div> -->
+		<textarea name="note-content" id="edit-note-content" cols="30" rows="10" v-model="NoteContent">{{ NoteContent }}</textarea>
+	</div>
+
+</template>
+
+<script>
+	export default {
+		name: 'Note',
+		data () {
+			return {
+				NoteId: '1',
+				NoteTitle: 'this is title',
+				NoteDate: 'this is date',
+				NoteContent: 'this is my first note'
+			}
+		},
+		methods: {
+			deleteNote () {
+				alert('delete note button');
+			},
+			editNote () {
+				alert('edit note button');
+			}
+		}
+	}
+</script>
+
+<style>
+	
+</style>
